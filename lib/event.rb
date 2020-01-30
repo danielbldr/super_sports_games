876ab3d92b957411da6665d416e4ba26f029f078
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-30 15:06:31 -0700

    Complete event.rb exercise

---
+++ lib/event.rb
@@ -0,0 +1,31 @@
+class Event
+  attr_reader :name, :ages
+  attr_accessor :largest_number, :smallest_number
+
+  def initialize(name, ages)
+    @name = name
+    @ages = ages
+
+  end
+
+  def max_age
+    @ages.max
+  end
+
+  def min_age
+    @ages.min
+  end
+
+  def average_age
+    @ages.sum/@ages.length
+  end
+
+  def standard_deviation_age
+    standard_dev = []
+    @ages.each do |age|
+      standard_dev << ((age - (@ages.sum/@ages.length)) ** 2)
+    end
+    Math.sqrt(standard_dev.sum/@ages.length).round(2)
+  end
+
+end
